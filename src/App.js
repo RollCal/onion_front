57c4e151a145ec94a930c292c444b0af81db8f73
JangSC9932 <klnhara@naver.com>
2024-05-31 14:21:40 +0900

    add : Layout 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import './App.css';
-import OnionVersusFlow from "./components/OnionVersusFlow";
 import Layout from "./components/Layout";
-import OnionVersus from "./components/OnionVersus";
+import {ChakraProvider} from '@chakra-ui/react'
 
 function App() {
+
     return (
-        <Layout>
-            <div style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-around",
-            }}>
-                <OnionVersus/>
-            </div>
-        </Layout>
+        <ChakraProvider>
+            <Layout />
+        </ChakraProvider>
     );
 }
 
