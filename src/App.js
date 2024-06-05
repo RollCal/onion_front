0c15798ae1419232b35c97b962bf24c4254c541d
Jungho Lee <awjdgh1111@gmail.com>
2024-06-05 21:43:28 +0900

    무한스크롤 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {ChakraProvider} from '@chakra-ui/react'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/route/Home";
 import Detail from "./components/route/Detail";
-import Header from "./Header";
 import {GlobalProvider} from "./components/GlobalState";
 import CreateVersus from "./components/route/CreateVersus";
 function App() {
--- src/components/onionversus/OnionVersus.js
+++ src/components/onionversus/OnionVersus.js
@@ -1,28 +1,30 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
 import axios from "axios";
 import OnionVersusFlow from "./OnionVersusFlow";
-import {Box} from "@chakra-ui/react";
 
 function OnionVersus(props) {
     const [versusList, setVersusList] = useState([]);
-    const [loading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true); // 더 이상 불러올 페이지가 있는지 여부
+
     const itemsPerPage = 3; // 백엔드에서 반환하는 항목 수에 맞춰 조정
 
-    async function getVersusList(pageNumber) {
+    const getVersusList = async (pageNumber) => {
         try {
-            const response = await axios.get(`/api/onions/onionlist?order=popular&page=${pageNumber}`);
-            return response.data.data;
+            setLoading(true);
+            const response = await axios.get(`/api/onions/onionlist?order=popular&search=&page=${pageNumber}`);
+            setVersusList(prevData => [...prevData,...response.data.data]);
+            setHasMore(response.data.meta.num_page > pageNumber); // 페이지 번호가 총 페이지 수보다 작거나 같으면 더 이상 불러올 페이지가 없음
+            setLoading(false);
         } catch (error) {
+            setLoading(false);
             console.error(error);
-            return [];
         }
-    }
+    };
 
     useEffect(() => {
-        getVersusList(page).then(data => {
-            setVersusList(prevData => [...prevData,...data]);
-        });
+        getVersusList(page);
     }, [page]);
 
     const observer = useRef();
@@ -30,22 +32,21 @@ function OnionVersus(props) {
         if (loading) return;
         if (observer.current) observer.current.disconnect();
         observer.current = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting) {
+            if (entries[0].isIntersecting && hasMore) {
                 setPage(prevPage => prevPage + 1);
             }
-        });
+        }, { threshold: 0.5 }); // 스크롤이 끝에 가까워졌을 때 콜백을 트리거
         if (node) observer.current.observe(node);
-    }, [loading]);
+    }, [loading, hasMore]);
 
     return (
-        <Box>
-            {
-                versusList.map((item, index) => (
-                    <OnionVersusFlow versus_data={item} key={index}/>
-                ))
-            }
+        <div>
+            {versusList.map((item, index) => (
+                <OnionVersusFlow versus_data={item} key={index} />
+            ))}
+            {loading && <p>Loading...</p>}
             <div ref={lastItemElementRef}></div>
-        </Box>
+        </div>
     );
 }
 
