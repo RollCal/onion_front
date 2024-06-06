5f37899ef1d45294860f7793cf03848f2a7f2e4a
Jungho Lee <awjdgh1111@gmail.com>
2024-06-06 20:11:52 +0900

    정렬버튼, 하이라이트(백엔드 조정 회의예정)

--- src/components/onionversus/OnionVersusFlow.js
+++ src/components/onionversus/OnionVersusFlow.js
@@ -1,19 +1,18 @@
-import React, {useEffect, useCallback} from 'react';
-import ReactFlow, {Controls, MiniMap, useEdgesState, useNodesState} from 'reactflow';
+import React, { useEffect, useCallback } from 'react';
+import ReactFlow, { Controls, useEdgesState, useNodesState } from 'reactflow';
 import 'reactflow/dist/style.css';
 import axios from "axios";
-import {useNavigate} from "react-router";
-import {Box, Grid, GridItem} from "@chakra-ui/react";
+import { useNavigate } from "react-router";
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
 
 function OnionVersusFlow(prop) {
     const [nodes, setNodes] = useNodesState([]);
     const [edges, setEdges] = useEdgesState([]);
-    const navigate = useNavigate()
-
+    const navigate = useNavigate();
 
     // 최상위 노드 생성
     const getVersusNodes = useCallback((versus_data) => {
-        const orange_onion = versus_data.orange_onion
+        const orange_onion = versus_data.orange_onion;
         const purple_onion = versus_data.purple_onion;
 
         // 하위 노드 생성
@@ -42,7 +41,7 @@ function OnionVersusFlow(prop) {
                         id: `e${onion.id}-${onion.parent_onion}`,
                         source: onion.id.toString(),
                         target: onion.parent_onion.toString(),
-                        markerStart: 'myCustomSvgMarker', markerEnd: {type: 'arrow', color: '#f00'},
+                        markerStart: 'myCustomSvgMarker', markerEnd: { type: 'arrow', color: '#f00' },
                     };
 
                     new_edge_list.push(newEdge);
@@ -53,39 +52,24 @@ function OnionVersusFlow(prop) {
                     id: onion.id.toString(),
                     sourcePosition: sourcePosition,
                     targetPosition: targetPosition,
-                    data: {label: onion.title},
-                    position: {x: 400 * new_node_list.length * loc_number + (200 * loc_number), y: 0},
+                    data: { label: onion.title },
+                    position: { x: 400 * new_node_list.length * loc_number + (200 * loc_number), y: 0 },
                 };
 
-                if (onion.color === "Purple") {
-                    newNode.style = {
-                        backgroundColor: "#9747FF",
-                        color: "white",
-                        height: "200px",
-                        width: "350px",
-                        display: "flex",
-                        justifyContent: "center",
-                        fontSize: "40px",
-                        paddingTop: "70px",
-                        borderRadius: "10px",
-                    }
-                } else {
-                    newNode.style = {
-                        backgroundColor: "#F24822",
-                        color: "white",
-                        height: "200px",
-                        width: "350px",
-                        display: "flex",
-                        justifyContent: "center",
-                        fontSize: "40px",
-                        paddingTop: "70px",
-                        borderRadius: "10px",
-                    }
-                }
+                newNode.style = {
+                    backgroundColor: onion.color === "Purple" ? "#9747FF" : "#F24822",
+                    color: "white",
+                    height: "200px",
+                    width: "350px",
+                    display: "flex",
+                    justifyContent: "center",
+                    fontSize: "40px",
+                    paddingTop: "70px",
+                    borderRadius: "10px",
+                };
 
                 new_node_list.push(newNode);
                 setNodes(prevNodes => [...prevNodes, newNode]);
-
 
                 if (onion.next) {
                     getNextNodes(onion.next.id, type, new_node_list, new_edge_list);
@@ -100,15 +84,14 @@ function OnionVersusFlow(prop) {
         getVersusNodes(prop.versus_data);
     }, [getVersusNodes]);
 
-    const defaultViewport = {x: 350, y: 150, zoom: 1};
-
+    const defaultViewport = { x: 350, y: 150, zoom: 1 };
 
     // 노드 상세 페이지 이동
     const onNodeClick = (event, node) => {
         navigate(`/onion/${node.id}`);
-    }
+    };
 
-    // 투표 그래프 와 versus 제목 레이아웃
+    // 투표 그래프와 versus 제목 및 하이라이트 레이아웃
     const VersusInfo = () => {
         const versus = prop.versus_data;
         const ov_title = versus.ov_title;
@@ -124,32 +107,35 @@ function OnionVersusFlow(prop) {
         }
         const total_up_vote_cnt = orange_up_vote_cnt + purple_up_vote_cnt;
 
+        const highlight = versus.highlight;
+
         return (
             <>
+                {highlight && (
+                    <Text color="red.500" fontSize="xl" mb="2">
+                        {highlight}
+                    </Text>
+                )}
                 <Grid>
                     <GridItem colSpan={1} w='100%' h='10'>
                         {ov_title}
                     </GridItem>
                 </Grid>
                 <Grid templateColumns={`repeat(${total_up_vote_cnt}, 1fr)`}>
-                    <GridItem colSpan={orange_up_vote_cnt} w='100%' h='10' bg='#F24822'
-                              color="white" p={2} pl={5}>
+                    <GridItem colSpan={orange_up_vote_cnt} w='100%' h='10' bg='#F24822' color="white" p={2} pl={5}>
                         {orange_onion_title}
                     </GridItem>
-                    <GridItem colSpan={purple_up_vote_cnt} w='100%' h='10' bg='#9747FF'
-                              align='right' color="white"
-                              p={2} pr={5}>
+                    <GridItem colSpan={purple_up_vote_cnt} w='100%' h='10' bg='#9747FF' align='right' color="white" p={2} pr={5}>
                         {purple_onion_title}
                     </GridItem>
                 </Grid>
             </>
-        )
-    }
-
+        );
+    };
 
     return (
         <Box p="10">
-            <VersusInfo/>
+            <VersusInfo />
             <div style={{
                 width: '70vw',
                 height: '500px',
@@ -165,7 +151,7 @@ function OnionVersusFlow(prop) {
                     defaultViewport={defaultViewport}
                     onNodeClick={onNodeClick}
                 >
-                    <Controls/>
+                    <Controls />
                 </ReactFlow>
             </div>
         </Box>
