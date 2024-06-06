a542cacae34c694e50baef9c8711c9a8461639d5
JangSC9932 <klnhara@naver.com>
2024-06-07 03:09:27 +0900

    create : 삭제/수정 버튼 추가 | 삭제 기능 추가

--- src/components/onionversus/Onions.js
+++ src/components/onionversus/Onions.js
@@ -4,9 +4,10 @@ import {useParams} from "react-router";
 import OnionFlow from "./OnionFlow";
 import CreateOnion from "./CreateOnion";
 import {Link} from "react-router-dom";
-import {Box, Button, Flex, Tooltip} from "@chakra-ui/react";
+import {Box, Button, ButtonGroup, Flex, Spacer, Tooltip} from "@chakra-ui/react";
 import {FaArrowUp, FaArrowDown} from "react-icons/fa";
 import {GlobalContext} from "../GlobalState";
+import {DeleteIcon, EditIcon} from "@chakra-ui/icons";
 
 function Onions(props) {
 
@@ -16,6 +17,7 @@ function Onions(props) {
     const [onion, setOnion] = useState();
     const [onion_tiele, setOnion_title] = useState();
     const [onion_color, setOnion_color] = useState();
+    const [onion_writer, setOnion_writer] = useState();
 
     const {isLoggedIn} = useContext(GlobalContext)
 
@@ -40,11 +42,14 @@ function Onions(props) {
             }
 
             // 댓글 등록 대상 업데이트
-            setOnion(onion_id);
             const response = await axios.get(`/api/onions/${onion_id}`, config);
+            console.log(response.data);
+            setOnion(onion_id);
             setOnion_title(response.data.title);
             setOnion_color(response.data.color);
             setIsVote(response.data.voted);
+            setOnion_writer(response.data.writer);
+
             // 댓글 목록 리턴
             return response.data.children;
         } catch (error) {
@@ -122,9 +127,46 @@ function Onions(props) {
             });
     }
 
+    const onionDeleteButtonHandler = (onion_id) => {
+        // 로컬스토리지에서 토큰 가져오기
+        const token = localStorage.getItem("token");
+
+        if (!token) {
+            alert("로그인이 필요합니다!");
+        }
+
+        // 헤더에 넣기
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        };
+
+        axios.delete(`/api/onions/${onion_id}`,config)
+            .then(function (response) {
+               alert("정상적으로 삭제요청하였습니다!");
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    if (error.response.data.code === 403) {
+                        alert("글 작성자가 아닙니다.");
+                    }else if (error.response.data.code === 400) {
+                        alert("이미 삭제요청하셨습니다.");
+                    }else {
+                        alert("서버에서 문제가 발생하였습니다.");
+                    }
+                }else {
+                    alert("서버에서 문제가 발생하였습니다.");
+                }
+            });
+    }
+
     useEffect(() => {
         // 처음 렌더링시 route에서 가져온 id 로 댓글 목록 불러오기
-        getVersusComment(param.onion_id).then((data => setOnionList(data)));
+        getVersusComment(param.onion_id).then((data) => {
+            setOnionList(data);
+        });
     }, [param.onion_id, isLoggedIn]); // 목록에있는 데이터들이 변경 될때마나 리랜더링, 공백일때는 무조건 한번 랜더링
 
 
@@ -134,66 +176,103 @@ function Onions(props) {
             {/*getVersusComment는 노드 클릭시 해당 노드의 댓글을 가져오기 위해 사용*/}
             {/*setOnionList는 댓글 목록 리렌더링 하기위해 사용*/}
             <OnionFlow onion_id={param.onion_id} key={param.onion_id} getVersusComment={getVersusComment}
-                       setOnionList={setOnionList}/>
+                       setOnionList={setOnionList} onionList={onionList}/>
             <Box>
                 <Box p={5}>
                     <Flex gap={2}>
-                        {
-                            isVote === "Up" ? (
+                        <Box>
+                            <ButtonGroup>
+                                {/* 투표했었던 건 구별되도록 설정 */}
+                                {
+                                    isVote === "Up" ? (
 
-                                    <Tooltip label='UP VOTE'>
-                                        <Button color="white" border='2px'
-                                                borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"} size='md'
-                                                bgColor={onion_color === "Orange" ? "#F24822" : "#9747FF"}
-                                                onClick={() => {
+                                            <Tooltip label='UP VOTE'>
+                                                <Button color="white" border='2px'
+                                                        borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        size='md'
+                                                        bgColor={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        onClick={() => {
+                                                            upVoteButtonHandler(onion, "Up")
+                                                        }}>
+                                                    {onion_tiele}
+                                                    <FaArrowUp/>
+                                                </Button>
+                                            </Tooltip>
+                                        )
+                                        :
+                                        (
+                                            <Tooltip label='UP VOTE'>
+                                                <Button color={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        border='2px'
+                                                        borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        size='md'
+                                                        bgColor="white" onClick={() => {
                                                     upVoteButtonHandler(onion, "Up")
                                                 }}>
-                                            {onion_tiele}
-                                            <FaArrowUp/>
-                                        </Button>
-                                    </Tooltip>
-                                )
-                                :
-                                (
-                                    <Tooltip label='UP VOTE'>
-                                        <Button color={onion_color === "Orange" ? "#F24822" : "#9747FF"} border='2px'
-                                                borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"} size='md'
-                                                bgColor="white" onClick={() => {
-                                            upVoteButtonHandler(onion, "Up")
-                                        }}>
-                                            {onion_tiele}
-                                            <FaArrowUp/>
-                                        </Button>
-                                    </Tooltip>
-                                )
-                        }
-                        {
-                            isVote === "Down" ? (
-                                    <Tooltip label='DOWN VOTE'>
-                                        <Button color="white" border='2px'
-                                                borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"} size='md'
-                                                bgColor={onion_color === "Orange" ? "#F24822" : "#9747FF"} onClick={() => {
-                                            upVoteButtonHandler(onion, "Down")
-                                        }}>
-                                            {onion_tiele}
-                                            <FaArrowDown/>
-                                        </Button>
-                                    </Tooltip>
-                                )
-                                :
-                                (
-                                    <Tooltip label='DOWN VOTE'>
-                                        <Button color={onion_color === "Orange" ? "#F24822" : "#9747FF"} border='2px'
-                                                borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"} size='md'
-                                                bgColor="white" onClick={() => {
-                                            upVoteButtonHandler(onion, "Down")
-                                        }}>
-                                            {onion_tiele}
-                                            <FaArrowDown/>
-                                        </Button>
-                                    </Tooltip>
-                                )
-                        }
+                                                    {onion_tiele}
+                                                    <FaArrowUp/>
+                                                </Button>
+                                            </Tooltip>
+                                        )
+                                }
+                                {
+                                    isVote === "Down" ? (
+                                            <Tooltip label='DOWN VOTE'>
+                                                <Button color="white" border='2px'
+                                                        borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        size='md'
+                                                        bgColor={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        onClick={() => {
+                                                            upVoteButtonHandler(onion, "Down")
+                                                        }}>
+                                                    {onion_tiele}
+                                                    <FaArrowDown/>
+                                                </Button>
+                                            </Tooltip>
+                                        )
+                                        :
+                                        (
+                                            <Tooltip label='DOWN VOTE'>
+                                                <Button color={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        border='2px'
+                                                        borderColor={onion_color === "Orange" ? "#F24822" : "#9747FF"}
+                                                        size='md'
+                                                        bgColor="white" onClick={() => {
+                                                    upVoteButtonHandler(onion, "Down")
+                                                }}>
+                                                    {onion_tiele}
+                                                    <FaArrowDown/>
+                                                </Button>
+                                            </Tooltip>
+                                        )
+                                }
+                            </ButtonGroup>
+                        </Box>
+                        <Spacer/>
+                        <Box>
+                            <ButtonGroup>
+                                <Tooltip label='DELETE ONION'>
+                                    <Button border='2px' size='md' bgColor="white" color="red.300"
+                                            borderColor="red.300"
+                                            marginTop="10px"
+                                            onClick={() => {
+                                                onionDeleteButtonHandler(onion);
+                                            }}>
+                                        <DeleteIcon/>
+                                    </Button>
+                                </Tooltip>
+                                <Tooltip label='EDIT ONION'>
+                                    <Button border='2px' size='md' bgColor="white" color="grey"
+                                            borderColor="grey"
+                                            marginTop="10px"
+                                            onClick={() => {
+                                                commentVoteButtonHandler(onion, "Down")
+                                            }}>
+                                        <EditIcon/>
+                                    </Button>
+                                </Tooltip>
+                            </ButtonGroup>
+                        </Box>
                     </Flex>
                 </Box>
                 <Box>
@@ -218,6 +297,27 @@ function Onions(props) {
                                     <FaArrowDown/>
                                 </Button>
                             </Tooltip>
+                            <Tooltip label='DELETE ONION'>
+                                <Button border='2px' size='xs' bgColor="white" color="red.300"
+                                        borderColor="red.300"
+                                        marginTop="10px"
+                                        onClick={() => {
+                                            onionDeleteButtonHandler(comment.id)
+                                        }}>
+                                    <DeleteIcon/>
+                                </Button>
+                            </Tooltip>
+                            <Tooltip label='EDIT ONION'>
+                                <Button border='2px' size='xs' bgColor="white" color="grey"
+                                        borderColor="grey"
+                                        marginTop="10px"
+                                        onClick={() => {
+                                            commentVoteButtonHandler(comment.id, "Down")
+                                        }}>
+                                    <EditIcon/>
+                                </Button>
+                            </Tooltip>
+
                         </Flex>
                     )}
                 </Box>
